12c36a2f60832e92a1caf278e7aef0b5a6c8a204
goblin01dev <35743828+goblin01dev@users.noreply.github.com>
2018-01-23 21:24:15 +0100

    Update MOD_WORKER.js

--- MOD_WORKER.js
+++ MOD_WORKER.js
@@ -1,6 +1,6 @@
+var i = document.getElementById("chat-box").style.borderRadius = "5px"; document.getElementById("chat-box").style.borderStyle = "dotted";
 function mod() {
   setInterval(()=>{
-    var i = document.getElementById("chat-box").style.borderRadius = "5px"; document.getElementById("chat-box").style.borderStyle = "dotted";
     postMessage(i);
   },1000)
 }
